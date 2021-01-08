47d54b970ba9d6251fae8b5c35630019dbdf31cc
masimacbook <masi19bw@gmail.com>
2021-01-08 15:03:26 +0900

    Set id

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/masibw/only-netfilter-queue
+
+go 1.14
+
+require (
+	github.com/AkihiroSuda/go-netfilter-queue v0.0.0-20201214173403-f140eb484571
+	github.com/google/gopacket v1.1.19 // indirect
+)
--- main.go
+++ main.go
@@ -9,14 +9,13 @@ import (
 func main() {
 	var err error
 
-	nfq, err := netfilter.NewNFQueue(0, 100, netfilter.NF_DEFAULT_PACKET_SIZE)
+	nfq, err := netfilter.NewNFQueue(2, 1000, netfilter.NF_DEFAULT_PACKET_SIZE)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	defer nfq.Close()
 	packets := nfq.GetPackets()
-
 	for true {
 		select {
 		case p := <-packets:
